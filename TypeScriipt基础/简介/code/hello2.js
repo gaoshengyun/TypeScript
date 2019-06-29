73d28f423a6adc790cee133be9bf9cfb5b037f86
gaoshengyun <18616242909@163.com>
2019-06-29 22:12:16 +0800

    code

---
+++ TypeScriipt基础/简介/code/hello2.js
@@ -0,0 +1,5 @@
+function sayHello2(person) {
+    return 'Hello, ' + person;
+}
+var user2 = [0, 1, 2];
+console.log(sayHello2(user2));
---
+++ TypeScriipt基础/简介/code/hello2.ts
@@ -0,0 +1,6 @@
+function sayHello2(person: string){
+  return 'Hello, ' + person
+}
+
+let user2 = [0,1,2]
+console.log(sayHello2(user2))
